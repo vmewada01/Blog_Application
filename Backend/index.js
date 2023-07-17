23a7e1ee246057a5c2312e8505ba9fde83d12521
Vishal Mewada <vishalmewada9826@gmail.com>
2023-07-17 11:58:41 +0530

    auth routes && modal schema added

--- Backend/index.js
+++ Backend/index.js
@@ -4,7 +4,12 @@ const cors = require("cors");
 
 const connection = require("./config/db")
 const authentication = require("./middlewares/authentication")
-
+const profileRouter = require("./routes/userProfile");
+const appRouter = require("./routes/blog");
+const blogsRouter = require("./routes/blogs");
+const userBlog = require("./routes/userBlog");
+const fileUpload = require("express-fileupload");
+const authRouter = require("./routes/auth");
 
 
 
@@ -14,10 +19,29 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use(
+    fileUpload({
+      useTempFiles: true,
+    })
+  );
 
 app.get("/", (req, res) => {
     res.send("Welcome to Backend server api");
   });
+
+
+  app.use("/auth", authRouter);
+
+  app.use("/blogs", blogsRouter);
+
+  app.use("/blog", userBlog);
+
+  app.use(authentication);
+
+
+
+
+
 
 const PORT= process.env.PORT || 8080
 
