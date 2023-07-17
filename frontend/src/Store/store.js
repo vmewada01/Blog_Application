e952a046a06220211a2159ef475105ce4567a276
Vishal Mewada <vishalmewada9826@gmail.com>
2023-07-17 13:21:30 +0530

    frontend_part_initialise

---
+++ frontend/src/Store/store.js
@@ -0,0 +1,11 @@
+import { applyMiddleware,componse, combineReducers, legacy_createStore } from "redux";
+
+
+import{reducer as authReducer} from "./Authentication/reducer"
+import {reducer as appReducer} from "./App/reducer"
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({authReducer,appReducer})
+
+export const store = legacy_createStore(rootReducer ,composeEnhancers(applyMiddleware(thunk)))
